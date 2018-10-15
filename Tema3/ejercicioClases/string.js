d762732b0e2bafcaa08d64142305e3a468b47451
AdrianSolano <adry.solfer@gmail.com>
2018-10-15 21:20:05 +0200

    Tercer commit

---
+++ Tema3/ejercicioClases/string.js
@@ -0,0 +1,23 @@
+function intercambiar(string){
+    let cadenafinal="";
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === string.charAt(i).toLocaleLowerCase() ) {
+            cadenafinal = cadenafinal + string.charAt(i).toLocaleUpperCase();
+        }else{
+            cadenafinal = cadenafinal + string.charAt(i).toLocaleUpperCase();
+        }
+    }
+    return cadenafinal;
+}
+
+function añadirCadena(string,posicion){
+    let cadenaAñadir= " de Pepe ";
+    for (let i = posicion; i <= string.length; i++) {
+        string = cadenaAñadir.concat(cadenaAñadir);
+    }
+    return string;
+}
+
+let frase = "Este Es EL ejemplO"
+//console.log(intercambiar(frase))
+console.log(añadirCadena(frase))
