80db9c29fa8e579f024f477d86283a6c26c84694
AdrianSolano <adry.solfer@gmail.com>
2018-11-26 21:16:18 +0100

    Jquery botones

--- Tema4/Jquery/dem.js
+++ Tema4/Jquery/dem.js
@@ -1,15 +1,43 @@
-let nodo = $('<div>Nuevo Nodo</div>')
+$(document).ready(
+    crearDivs()
+);
 
+function quitarDivs(){
+
+    if (div.find("input")){
+        $("div").empty();
+    }else{
+        $("div").empty();
+    }
+
+}
 
 function crearDivs() {
-    let body = document.body;
-    let divs= $("div");
-    for(let i = 1 ; i < divs; i++){
-    $(div[i]).text("TEXTO DEL NODO"+i);
-}
+    
+    for(let i = 1; i <= 10; i++){
+        let div = $('<div></div>');
+        let h1 = $('<h1></h1>');
+        h1.text("Div Nº "+i);
+        div.append(h1);
+        $(document.body).append(div);
+        for(let j = 1; j <= i; j++){
+            let input = $('<input></input>');
+            $(div).append(input);
+            input.val(i+"."+j);
+            input.attr("name","nodo"+i+j);
+            if ( j % 2 || i % 2 ){
+                input.addClass("azul");
+            }else{
+                input.addClass("naranja");
+            }
+            div.find("input")
+
+
+        }
+        
+    }
 
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
 
-});
+
